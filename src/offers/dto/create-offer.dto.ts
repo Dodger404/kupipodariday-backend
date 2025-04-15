11d24e4ad472f55fcac6d4622c47b232589af870
Kirill Bulgakov <bilbo.baggins.from.sheer@gmail.com>
2025-04-15 17:45:26 +0500

    реализация задания

---
+++ src/offers/dto/create-offer.dto.ts
@@ -0,0 +1,10 @@
+import { IsBoolean, IsNumber, IsPositive } from 'class-validator';
+
+export class CreateOfferDto {
+  @IsNumber()
+  @IsPositive()
+  amount: number;
+
+  @IsBoolean()
+  hidden: boolean;
+}
